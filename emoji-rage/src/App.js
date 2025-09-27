953a6a3b28eb306f735b3e5c12091f3ec4bc4181
Mansi Saini <mansisaini26@gmail.com>
2025-09-27 16:57:15 -0400

    styling for login

--- emoji-rage/src/App.js
+++ emoji-rage/src/App.js
@@ -1,28 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import useFirebaseMessaging from './hooks/useFirebaseMessaging';
+import * as React from "react";
+import { Input } from "baseui/input";
+import { useStyletron } from "baseui";
+import {
+  DisplayLarge,
+  DisplayMedium,
+  DisplaySmall,
+  DisplayXSmall,
+} from "baseui/typography";
+
+const textString = "We ignite opportunity by setting the world in motion.";
+
 
 function App() {
-  useFirebaseMessaging();
+  const [username, setUsername] = React.useState("");
+  const [password, setPassword] = React.useState("");
+  const [css, theme] = useStyletron();
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="App" style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Dev Help</h1>
 
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Input
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Type your username..."
+        clearOnEscape
+        overrides={{
+          Root: {
+            style: {
+              maxWidth: "300px",
+              margin: "20px auto",
+              borderRadius: "5px",
+            },
+          },
+        }}
+      />
+      <p>Current value: {username}</p>
+      <Input
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Type your password..."
+        clearOnEscape
+        overrides={{
+          Root: {
+            style: {
+              maxWidth: "300px",
+              margin: "20px auto",
+              borderRadius: "5px",
+            },
+          },
+        }}
+      />
+
+      
     </div>
   );
 }
 
 export default App;
+
--- emoji-rage/src/index.js
+++ emoji-rage/src/index.js
@@ -4,14 +4,24 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Client as Styletron } from 'styletron-engine-atomic';
+import { Provider as StyletronProvider } from 'styletron-react';
+import { BaseProvider, LightTheme } from 'baseui';
+import 'normalize.css';
+
+// Create the Styletron engine
+const engine = new Styletron();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <StyletronProvider value={engine}>
+      <BaseProvider theme={LightTheme}>
+        <App />
+      </BaseProvider>
+    </StyletronProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// Measure performance
 reportWebVitals();
